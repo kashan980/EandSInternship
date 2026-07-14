9a3eca51f2fa406daa6518649e8722e98af7d305
Muhammad Hammad <smuhammadhammad005@gmail.com>
2026-07-14 16:38:59 +0500

    changes

--- lib/presentation/menu/menu.dart
+++ lib/presentation/menu/menu.dart
@@ -8,7 +8,12 @@ class Menu extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Menu")),
       body: Center(
-        child: AppButton(text: "Click Me", backgroundColor: Colors.green),
+        child: AppButton(
+          text: "Click Me",
+          backgroundColor: Colors.green,
+          borderColor: Colors.black,
+          textColor: Colors.white,
+        ),
       ),
     );
   }
@@ -18,24 +23,31 @@ class AppButton extends StatelessWidget {
   final String text;
   final Color backgroundColor;
   final Color textColor;
+  final Color borderColor;
   final double borderRadius;
-
+  final bool isEnabled;
+  final bool showIcon;
   const AppButton({
     super.key,
     required this.text,
     this.backgroundColor = Colors.red,
     this.textColor = Colors.white,
     this.borderRadius = 8.0,
+    this.borderColor = Colors.black,
+    this.isEnabled = false,
+    this.showIcon = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(SnackBar(content: Text('Button "$text" tapped!')));
-      },
+      onTap: isEnabled
+          ? () {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text('Button "$text" tapped!')));
+            }
+          : null,
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
         decoration: BoxDecoration(
@@ -45,7 +57,7 @@ class AppButton extends StatelessWidget {
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Icon(Icons.touch_app, color: textColor),
+            if (showIcon) Icon(Icons.touch_app, color: textColor),
             const SizedBox(width: 10),
             Text(
               text,
