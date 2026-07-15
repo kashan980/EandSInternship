e94f3216f629137ca82bef6810cba2d310553fb6
kashan980 <53686@students.riphah.edu.pk>
2026-07-15 12:15:52 +0500

    Configured Firebase for the project and initialized it in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
+import 'package:firebase_core/firebase_core.dart'; // Added Firebase Core import
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'config/routes/app_routes.dart';
-import 'presentation/main_screen/main_screen.dart';
-import 'presentation/menu/menu.dart';
 
-void main() {
+import 'config/routes/app_routes.dart';
+import 'firebase_options.dart'; // Added Firebase Options import
+
+void main() async {
+  // Ensures Flutter framework is fully initialized before calling native code
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initializes Firebase using the configuration for the current platform
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({super.key}); // Added a standard key constructor
+
   final _router = GoRouter(
     routes: $appRoutes,
     navigatorKey: navigatorKey,
     initialLocation: getAppInitialRouteLocation(),
   );
 
-
   @override
-  Widget build(BuildContext context) => MaterialApp.router(
-    routerConfig: _router,
-  );
+  Widget build(BuildContext context) =>
+      MaterialApp.router(routerConfig: _router);
 }
