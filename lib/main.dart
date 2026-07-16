161eace4832171a3bf744314993bcf3cf4d4f9e5
kashan980 <53686@students.riphah.edu.pk>
2026-07-16 14:34:21 +0500

    Integrated Firebase Crashlytics and added test crash button to MainScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:ui'; // Added for PlatformDispatcher
+
 import 'package:firebase_core/firebase_core.dart'; // Added Firebase Core import
+import 'package:firebase_crashlytics/firebase_crashlytics.dart'; // Added Crashlytics import
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
 import 'config/routes/app_routes.dart';
 import 'firebase_options.dart'; // Added Firebase Options import
 
@@ -10,6 +14,15 @@ void main() async {
 
   // Initializes Firebase using the configuration for the current platform
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Pass all uncaught "fatal" errors from the Flutter framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   runApp(MyApp());
 }
